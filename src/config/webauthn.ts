a2dd3ee796a3155458a9b4b357233bad252b6706
Pedro Villafane <tuemailreal@correo.com>
2026-02-27 14:53:23 -0400

    Fix rpID for production domain

--- src/config/webauthn.ts
+++ src/config/webauthn.ts
@@ -1,3 +1,11 @@
-export const rpID = "localhost";
-export const origin = "http://localhost:3000";
+export const rpID =
+  process.env.NODE_ENV === "production"
+    ? "portal-paciente-orpin.vercel.app"
+    : "localhost";
+
+export const origin =
+  process.env.NODE_ENV === "production"
+    ? "https://portal-paciente-orpin.vercel.app"
+    : "http://localhost:3000";
+
 export const rpName = "Portal del Paciente";
