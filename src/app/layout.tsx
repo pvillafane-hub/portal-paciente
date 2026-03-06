4173ab8250ff619606d344912f3c422904707114
Pedro Villafane <tuemailreal@correo.com>
2026-03-06 12:02:17 -0400

    fix layout pathname type

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
 
-  const pathname = usePathname()
+  const pathname = usePathname() ?? ""
 
   // páginas públicas
   const publicPages = ["/", "/login", "/signup", "/recovery"]
